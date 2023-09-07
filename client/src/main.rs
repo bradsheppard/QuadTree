15a35e0dda64f4ba1ce18dbc96ac9b918611acae
Brad Sheppard <bradsheppard22@gmail.com>
2023-09-06 23:08:53 -0230

    Refactored client CLI

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,68 +1,71 @@
-mod error;
-
-use proto::{QuadClient, AddPointRequest, Point};
+use proto::{QuadClient, AddPointRequest, Point, DeletePointRequest};
+use clap::{Parser, Subcommand, Args};
 use tonic::transport::Channel;
+use anyhow::{Result, anyhow};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    AddPoint(InputPoint),
+    DeletePoint(InputPoint)
+}
+
+#[derive(Args)]
+struct InputPoint {
+    x: i64,
+    y: i64
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let command = std::env::args().nth(1);
-    let point = std::env::args().nth(2);
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
 
-    match (command, point) {
-        (Some(c), Some(p)) => {
-            let result = process_command(c.as_str(), p.as_str()).await;
-            
-            match result {
-                Ok(_) => {
-                    println!("Executed successfully");
+    match &cli.command {
+        Commands::AddPoint(point) => {
+            let request = tonic::Request::new(AddPointRequest{
+                    point: Some(Point{
+                        x: point.x,
+                        y: point.y
+                    })
                 }
-                Err(e) => {
-                    println!("Error processing command {}", e);
+            );
+
+            let mut client = get_client().await?;
+
+            let _response = client.add_point(request)
+                .await
+                .map_err(|_x| anyhow!("Failure adding point"))?;
+        },
+        Commands::DeletePoint(point) => {
+            let request = tonic::Request::new(DeletePointRequest{
+                    point: Some(Point{
+                        x: point.x,
+                        y: point.y
+                    })
                 }
-            }
-        }
-        _ => {
-            println!("Unknown command");
+            );
+
+            let mut client = get_client().await?;
+
+            let _response = client.delete_point(request)
+                .await
+                .map_err(|_x| anyhow!("Failure deleting point"))?;
         }
     }
 
     Ok(())
 }
 
-async fn process_command(command: &str, point: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let client = QuadClient::connect("http://[::1]:50051").await;
+async fn get_client() -> Result<QuadClient<Channel>> {
+    let client = QuadClient::connect("http://[::1]:50051")
+        .await
+        .map_err(|_x| anyhow!("Cannot connect to service"))?;
 
-    match client {
-        Ok(mut c) => process(&mut c, command, point).await,
-        Err(e) => Err(Box::new(error::ErrorStatusError::new(e.to_string().as_str())))
-    }
-
+    return Ok(client);
 }
 
-async fn process(client: &mut QuadClient<Channel>, command: &str, point: &str) -> Result<(), Box<dyn std::error::Error>> {
-    match command {
-        "addpoint" => {
-            let request = tonic::Request::new(AddPointRequest{
-                    point: Some(Point{
-                        x: 1,
-                        y: 2
-                    })
-                }
-            );
-
-            let response = client.add_point(request).await;
-
-            match response {
-                Ok(_) => {
-                    Ok(())
-                },
-                Err(e) => {
-                    Err(Box::new(error::ErrorStatusError::new(e.message())))
-                }
-            }
-        }
-        _ => {
-            Err(Box::new(error::InvalidCommandError))
-        }
-    }
-}
